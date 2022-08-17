a1ab2624baf0ef6e27f1989311b10e686179c85f
jhonatan.salazar <jsshurtado19@gmail.com>
2022-08-17 08:34:08 -0500

    Initial commit

---
+++ src/files/helpers/fileNamer.helper.ts
@@ -0,0 +1,14 @@
+export const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  callback: (error: Error | null, acceptFile: boolean) => void,
+) => {
+  if (!file) return callback(new Error('File is empty'), false);
+
+  const fileExtension: string = file.mimetype.split('/')[1];
+  const validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+  if (validExtensions.includes(fileExtension)) return callback(null, true);
+
+  callback(null, false);
+};
